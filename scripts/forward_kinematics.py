9284bc04e0046a32520158b070a91ff11af3480d
Gonzo Denetro <gonzi.ceron.denetro@gmail.com>
2026-04-27 13:33:01 -0600

    Draw robot

--- scripts/forward_kinematics.py
+++ scripts/forward_kinematics.py
@@ -20,16 +20,24 @@ pygame.display.set_caption('Differential Robot - Forward Kinematics')
 #Main function
 def main():
     #Robot Configuration
-    init_pos = [10, 10]
+    robot_x = 10
+    robot_y = 10
     robot_width = 20
     robot_height = 20
-    robot = pygame.Rect(init_pos[0], init_pos[1], robot_width, robot_height)
+    robot = pygame.Rect(robot_x, robot_y, robot_width, robot_height)
+    
+    #Robot Wheel Velocities
     
     running = True
     
     while running:
         #Get INputs
         for event in pygame.event.get():
+            
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_RIGHT]:
+                robot.x += 3
+            
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
@@ -38,10 +46,13 @@ def main():
         
         
         #Render Elements
+        window.fill((255, 255, 255)) #Erase window
+        pygame.draw.rect(window, (255, 30, 70), robot) #Draw robot
+        
         pygame.display.update()
         clock.tick(FPS)
         
-        pygame.draw.rect(window, (255, 30, 70), robot)
+    
 
 
 if __name__ == '__main__':

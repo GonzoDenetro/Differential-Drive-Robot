449e924ca49d02c7cce30df6f9e2b3f2fc067f86
Gonzo Denetro <gonzi.ceron.denetro@gmail.com>
2026-04-27 12:55:28 -0600

    Pygame basic structure

--- scripts/forward_kinematics.py
+++ scripts/forward_kinematics.py
@@ -19,6 +19,12 @@ pygame.display.set_caption('Differential Robot - Forward Kinematics')
 
 #Main function
 def main():
+    #Robot Configuration
+    init_pos = [10, 10]
+    robot_width = 20
+    robot_height = 20
+    robot = pygame.Rect(init_pos[0], init_pos[1], robot_width, robot_height)
+    
     running = True
     
     while running:
@@ -34,6 +40,8 @@ def main():
         #Render Elements
         pygame.display.update()
         clock.tick(FPS)
+        
+        pygame.draw.rect(window, (255, 30, 70), robot)
 
 
 if __name__ == '__main__':
